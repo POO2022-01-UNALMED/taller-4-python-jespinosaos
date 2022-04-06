a91b7f0019e429f0d5b810d300e318ed7689b9b1
jespinosaos <101433694+jespinosaos@users.noreply.github.com>
2022-04-05 22:09:53 -0500

    fgctyfv

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,1 +1,9 @@
-a
+class Asignatura:
+
+    def __init__(self, nombre, salon="remoto"):
+      self._nombre = nombre
+      self._salon = salon
+    def __str__(self):
+      cadena=self._nombre+" " + self._salon
+      return cadena
+
---
+++ classroom/grupo.py
@@ -0,0 +1,35 @@
+from asignatura import Asignatura
+
+class Grupo:
+
+    grado = "Grado 12"
+
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
+        self._grupo = grupo
+        self._asignaturas = asignaturas
+        self.listadoAlumnos = estudiantes
+    def __str__(self):
+      cadena= "Grupo de estudiantes: " + self._grupo
+      return cadena
+
+    def listadoAsignaturas(self, **kwargs):
+        for asasa in kwargs.values():
+          
+          self._asignaturas.append(Asignatura(asasa))
+    
+    def agregarAlumno(self, alumno, lista=None):
+      if(lista is None):
+        lista=[]
+      lista.append(alumno)
+      self.listadoAlumnos = self.listadoAlumnos + lista
+      
+   
+     
+
+    @classmethod
+    def asignarNombre(*args):
+      for arg in args:
+        if arg == "Grado 1":
+          Grupo.grado="Grado 1"
+        else:
+          Grupo.grado="Grado"
